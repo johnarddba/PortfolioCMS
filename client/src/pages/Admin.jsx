d0250a009ac9049d83272365fbe150b556e60a6f
johnarddba <johnard24@yahoo.com>
2026-07-20 23:36:21 +0800

    new

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -3,31 +3,52 @@ import { useEffect, useState } from "react";
 function Admin() {
   const [projects, setProjects] = useState([]);
 
-  useEffect(() => {
+  const loadProjects = () => {
     fetch("http://localhost:5000/api/projects")
       .then((res) => res.json())
-      .then((data) => setProjects(data));
-  }, []);
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setProjects(data);
+        } else {
+          console.error(data);
+        }
+      });
+  };
+
+useEffect(() => {
+    fetch("http://localhost:5000/api/projects")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("API Response:", data);
+            setProjects(data);
+        })
+        .catch((err) => console.error(err));
+}, []);
 
   return (
     <div className="container py-5">
-      <h1>Portfolio CMS</h1>
+      <h1>Portfolio CMS Admin</h1>
 
       <hr />
 
-      <h3>Projects</h3>
-
       {projects.map((project) => (
-        <div key={project.id} className="border p-3 mb-3">
-          <h4>{project.title}</h4>
+        <div
+          key={project.id}
+          className="card p-3 mb-3 shadow-sm"
+        >
+          <h3>{project.title}</h3>
 
           <p>{project.description}</p>
 
-          <small>{project.technology}</small>
+          <strong>{project.technology}</strong>
 
           <br />
 
-          <a href={project.github}>
+          <a
+            href={project.github}
+            target="_blank"
+            rel="noreferrer"
+          >
             Github
           </a>
         </div>
